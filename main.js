fc5d25e6a552780121b4e6a0b394e9a4fe7b2ba1
KHayotova1 <sitorahayotova7455@mail.ru>
2024-03-18 17:02:01 +0500

    sadasd

---
+++ main.js
@@ -0,0 +1,15 @@
+const sr = ScrollReveal ({
+    distance: '65px',
+    duration: 2600,
+    delay: 450,
+    reset: true
+});
+sr.reveal('.hero-text',{delay:200,origin:'top'});
+sr.reveal('.icons',{delay:450,origin:'top'});
+sr.reveal('.hero-text',{delay:500,origin:'left'});
+sr.reveal('.scroll-down',{delay:500,origin:'right'});
+$(".option").click(function(){
+    $(".option").removeClass("active");
+    $(this).addClass("active");
+    
+ });
